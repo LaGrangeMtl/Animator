73186456b1c75eb4e68ff19c4fdcee4f3b9021b2
Maxime Des Roches <m.desroches@la-grange.ca>
2020-07-16 15:08:26 -0400

    Added svg capabilities

--- src/functions/parseRelativeVal.js
+++ src/functions/parseRelativeVal.js
@@ -4,9 +4,11 @@
  * @param {HTMLElement} elem 
  */
 export default function parseRelativeVal(prop, value, elem) {
+	const rect = elem.getBoundingClientRect();
+
 	switch (prop) {
-		case 'y': return (value / 100) * elem.clientHeight;
+		case 'y': return (value / 100) * rect.height;
 		case 'x':
-		default: return (value / 100) * elem.clientWidth;
+		default: return (value / 100) * rect.width;
 	}
 }
