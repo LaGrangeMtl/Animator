9ed6f904e91a3356c30f8ff259f7a1f02c445423
Maxime Des Roches <m.desroches@la-grange.ca>
2020-08-19 16:56:09 -0400

    performance css vars => attr

--- src/functions/getContextScrollTop.js
+++ src/functions/getContextScrollTop.js
@@ -6,7 +6,7 @@ export default function getContextScrollTop(context) {
 	let ctx = context;
 	if (context.tagName === 'HTML') ctx = document.scrollingElement || context;
 	
-	const stProp = context.style.getPropertyValue('--scroll-top');
+	const stProp = context.getAttribute('data-scroll-top') || context.style.getPropertyValue('--scroll-top');
 
 	return (stProp || stProp !== '') ? parseFloat(-stProp) : ctx.scrollTop;
 }
