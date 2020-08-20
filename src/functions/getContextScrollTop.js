1ac7e4d7dd3cccf9975d49989c908a07035b32d9
Maxime Des Roches <m.desroches@la-grange.ca>
2020-08-20 11:53:47 -0400

    with upadte to scroller, no need to get scroll-top on resize, we can use base positions

--- src/functions/getContextScrollTop.js
+++ src/functions/getContextScrollTop.js
@@ -5,8 +5,6 @@
 export default function getContextScrollTop(context) {
 	let ctx = context;
 	if (context.tagName === 'HTML') ctx = document.scrollingElement || context;
-	
-	const stProp = context.getAttribute('data-scroll-top') || context.style.getPropertyValue('--scroll-top');
 
-	return (stProp || stProp !== '') ? parseFloat(-stProp) : ctx.scrollTop;
+	return ctx.scrollTop;
 }
